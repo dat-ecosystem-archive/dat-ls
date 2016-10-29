bd72c7407e80343d77a8080861ad195673502177
Mathias Buus <mathiasbuus@gmail.com>
2016-10-29 13:50:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# dat-ls
+
+Small program that lists all the changes in a dat
+
+```
+npm install -g dat-ls
+```
+
+## Usage
+
+```
+dat-ls <dat-link>
+```
+
+Here is an example
+
+```
+> dat-ls 8a1b6864f290a1204a3fe048ced34865a24f5fdef4ce3753bab613f66636587d
+
+Dat contains 6 changes
+
+[index] content --> bc65a6a4f93e6d400f94f9bb3f8ef6ae31e2affb14bcf5253819ab12adda8d6e
+[dir]   (empty)
+[file]  LICENSE (1.08 kB, 1 blocks)
+[file]  README.md (145 B, 1 blocks)
+[file]  index.js (1.18 kB, 1 blocks)
+[file]  package.json (648 B, 1 blocks)
+
+Total content size: 3.05 kB
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+var hypercore = require('hypercore')
+var swarm = require('hyperdrive-archive-swarm')
+var encoding = require('hyperdrive-encoding')
+var prettyBytes = require('pretty-bytes')
+var memdb = require('memdb')
+
+var key = process.argv[2]
+
+if (!key) {
+  console.error('Usage: dat-ls [key]')
+  process.exit(1)
+}
+
+var changes = hypercore(memdb())
+var size = 0
+
+var feed = changes.createFeed(key)
+
+swarm(feed)
+
+var rs = feed.createReadStream()
+
+rs.once('data', function () {
+  console.log('Dat contains %d changes\n', feed.blocks)
+})
+
+rs.on('data', function (data) {
+  data = encoding.decode(data)
+
+  if (data.type === 'file') {
+    size += data.length
+  }
+
+  switch (data.type) {
+    case 'index': return console.log('[index] content --> %s', data.content ? data.content.toString('hex') : '(nil)')
+    case 'file': return console.log('[file]  %s (%s, %s blocks)', data.name, prettyBytes(data.length), data.blocks)
+    case 'directory': return console.log('[dir]   %s', data.name || '(empty)')
+  }
+
+  console.log('[' + data.type + ']', data)
+})
+
+rs.on('end', function () {
+  console.log()
+  console.log('Total content size: %s', prettyBytes(size))
+  process.exit(0)
+})
