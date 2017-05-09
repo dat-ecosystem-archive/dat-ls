8ce5d10b53b64db3eab6630c20fabe098440719a
Joe Hand <joe@hand.email>
2017-05-09 16:19:12 -0700

    remove summary stuff

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ var swarm = require('hyperdiscovery')
 var hyperdrive = require('hyperdrive')
 var prettyBytes = require('pretty-bytes')
 var ram = require('random-access-memory')
-var progress = require('progress-string')
-var ansi = require('ansi-diff-stream')
 
 var key = process.argv[2]
-var summary = process.argv.indexOf('--summary') > -1 || process.argv.indexOf('-s') > -1
-var live = !summary && process.argv.indexOf('--live') > -1 || process.argv.indexOf('-l') > -1
+var live = process.argv.indexOf('--live') > -1 || process.argv.indexOf('-l') > -1
 
 if (!key) {
   console.error('Usage: dat-ls [key]')
@@ -25,10 +22,6 @@ archive.ready(function () {
 })
 
 function run () {
-  var stream = ansi()
-  var bar = null
-  var cnt = 0
-  var pad = ''
   var zeros = '000000000000'
   var files = {}
 
@@ -44,23 +37,6 @@ function run () {
     if (digits !== Math.ceil(digits)) digits = Math.ceil(digits)
     else digits++
     zeros = zeros.slice(0, digits)
-  }
-
-  if (summary) {
-    bar = progress({width: 60, total: archive.metadata.length, style: function (a, b) { return a + '>' + b }})
-    stream.pipe(process.stdout)
-    update()
-  }
-
-  function update () {
-    var first = 'Total content size: ' + prettyBytes(size)
-    if (first.length > pad.length) pad = Array(first.length + 1).join(' ')
-    first += pad.slice(first.length)
-
-    stream.write(
-      '[' + bar(cnt++) + ']\n\n' +
-      first + ' (' + prettyBytes(archive.metadata.byteLength) + ' metadata)'
-    )
   }
 
   rs.on('data', function (data) {
@@ -79,11 +55,6 @@ function run () {
       delete files[data.name]
     }
 
-    if (summary) {
-      update()
-      return
-    }
-
     switch (data.type) {
       case 'put': return console.log(s + ' [put]  %s (%s, %s %s)', data.name, prettyBytes(data.value.size), data.value.blocks, data.value.blocks === 1 ? 'block' : 'blocks')
       case 'del': return console.log(s + ' [del]  %s', data.name || '(empty)')
@@ -93,10 +64,8 @@ function run () {
   })
 
   rs.on('end', function () {
-    if (!summary) {
-      console.log()
-      console.log('Total content size: %s (%s metadata)', prettyBytes(size), prettyBytes(archive.metadata.byteLength))
-    }
+    console.log()
+    console.log('Total content size: %s (%s metadata)', prettyBytes(size), prettyBytes(archive.metadata.byteLength))
     process.exit(0)
   })
 }
